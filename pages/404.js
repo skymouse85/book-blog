c5d1b4fa8183fc7528160d5edeea66133525e9cc
Mark Clifford <markmclifford@gmail.com>
2023-04-08 13:13:10 -0700

    first blog push

---
+++ pages/404.js
@@ -0,0 +1,5 @@
+import utilStyles from '../styles/utils.module.css'
+
+export default function Custom404() {
+    return <h1 className={utilStyles.errorPage}>404 - Page not found 📜🕳</h1>
+}
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import Layout, { siteTitle} from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
+import Date from '../components/date';
 import { getSortedPostsData } from '../lib/posts';
 
 export async function getStaticProps() {
@@ -20,22 +21,22 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello! My name is Mark Pascucci-Clifford. I'm a software engineer, vibraphonist, and composer</p>
-        <p>
+        <p>Hello! My name is Mark Pascucci-Clifford. I'm an aspiring web developer, vibraphonist, and composer</p>
+        {/* <p>
         (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+        </p> */}
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>{title}</Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date}/>
+              </small>
             </li>
           ))}
         </ul>
