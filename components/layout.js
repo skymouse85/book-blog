a70ae109af48a0be56b3ee43a8173d019683d6db
Mark Clifford <markmclifford@gmail.com>
2023-04-08 13:27:13 -0700

    import bug fix

--- components/layout.js
+++ components/layout.js
@@ -3,10 +3,10 @@ import Head from 'next/head';
 import Image from 'next/image';
 import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
-import Link from 'next/Link';
+import Link from 'next/link';
 
 const name = 'Mark Pascucci-Clifford';
-export const siteTitle = 'Next.js Sample Website';
+export const siteTitle = 'Book Smark';
 
 export default function Layout({ children,  home }) {
     return (
