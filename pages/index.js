f42d7a7adee0a51af92285cc7ab922cc5d4c2735
Mark Clifford <markmclifford@gmail.com>
2023-04-08 13:48:05 -0700

    index update

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello! My name is Mark Pascucci-Clifford. I'm an aspiring web developer, vibraphonist, and composer</p>
+        <p>Hello! My name is Mark Pascucci-Clifford. I'm an aspiring web developer, vibraphonist, and composer.</p>
         {/* <p>
         (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
